f8c7b8a9b5c0fbb31ab360e1d09a7553ac576879
Ryan Rudes <ryanrudes@gmail.com>
2020-12-11 15:50:35 -0500

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,21 +1,30 @@
-# Not the most efficient solution, but it passes all of the test cases in time.
+def search(l, n):
+  if len(l) == 1:
+    return l[0]
 
-import sys
+  i = len(l) // 2
+  x = l[i]
 
-def is_palindrome(num):
-    return str(num) == str(num)[::-1]
+  if n > x:
+    return search(l[i:], n)
+  elif n == x:
+    return l[i - 1]
+  else:
+    return search(l[:i], n)
 
-T = int(input().strip())
+palindrome = lambda n: str(n) == str(n)[::-1]
+products = []
+
+for x in range(100, 1000):
+  for y in range(100, 1000):
+    product = x * y
+    if palindrome(product):
+      products.append(product)
+
+products = list(set(products))
+products.sort()
+
+T = int(input())
 for i in range(T):
-    N = int(input().strip())
-    
-    maxPalindrome = 0
-
-    for x in reversed(range(100, 1000)):
-        for y in reversed(range(100, x + 1)):
-            prod = x * y
-            
-            if prod < N and prod > maxPalindrome and is_palindrome(prod):
-                maxPalindrome = prod
-                
-    print (maxPalindrome)
+    N = int(input())
+    print (search(products, N))
